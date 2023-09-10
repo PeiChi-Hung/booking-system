e1933d64a0a9330493bdde979b2f598861a72765
Patty Hung <pattyhung72@gmail.com>
2023-09-10 15:54:11 +1000

    Added notification badge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "47e9c9062e311b14"; }
+/******/ 		__webpack_require__.h = function() { return "2b43de0008cf10ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,55 @@ import CancelledTask from "@/components/CancelledTask"
 import PauseTask from "@/components/PauseTask"
 import PaidTask from "@/components/PaidTask"
 import UnpaidTask from "@/components/UnpaidTask"
+import { Badge } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
+
+// following numbers will be replaced by API
+const runningCount = 4
+const pauseCount = 4
+const unpaidCount = 4
+const paidCount = 4
+const cancelledCount = 4
+const failedCount = 4
 
 export default function Home() {
   return (
     <main className="mt-3 px-3 md:mt-5 md:px-5">
       <Tabs defaultValue="running">
         <TabsList className="grid grid-cols-3 grid-rows-2 md:grid-cols-6 md:grid-rows-none w-full gap-3 h-full">
-          <TabsTrigger value="running">Running</TabsTrigger>
-          <TabsTrigger value="pause">Pause</TabsTrigger>
+          <TabsTrigger value="running">
+            Running
+            <Badge
+              className={cn(
+                runningCount > 0 ? "ml-2 bg-green-400" : "ml-2 bg-transparent"
+              )}
+            >
+              {runningCount}
+            </Badge>
+          </TabsTrigger>
+          <TabsTrigger value="pause">
+            Pause
+            <Badge
+              className={cn(
+                pauseCount > 0 ? "ml-2 bg-yellow-300" : "ml-2 bg-transparent"
+              )}
+            >
+              {pauseCount}
+            </Badge>
+          </TabsTrigger>
           <TabsTrigger value="unpaid">Unpaid</TabsTrigger>
           <TabsTrigger value="paid">Paid</TabsTrigger>
           <TabsTrigger value="cancelled">Cancelled</TabsTrigger>
-          <TabsTrigger value="failed">Failed</TabsTrigger>
+          <TabsTrigger value="failed">
+            Failed
+            <Badge
+              className={cn(
+                failedCount > 0 ? "ml-2 bg-red-500" : "ml-2 bg-transparent"
+              )}
+            >
+              {failedCount}
+            </Badge>
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="running">
           <div className="my-5">
